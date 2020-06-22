f175dfad6d769582f565a933ff8aac2152234f79
Arslan Ameen <66115871+arslanvirk@users.noreply.github.com>
2020-06-22 23:09:02 +0500

    Header Update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <header>
-            <h2>Expense Tracker Develop by <a href="https://www.linkedin.com/in/muhammad-arslan-ameen">Arslan Ameen</a></h2>
+            <h2>Bootcamp 2020 Ninjas - Expense Tracker App Developed by <a href="https://www.linkedin.com/in/muhammad-arslan-ameen">Arslan Ameen</a></h2>
         </header>
     )
 }
