bd5b88977e53b8bf265257926264edf0bf552ce5
Arslan Ameen <arslan.virk11@gmail.com>
2020-09-21 21:08:30 +0500

    Project7a

---
+++ build/precache-manifest.3aefcd049b56a92d98dcee7d730b9c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33076925c2813a1ccd89482fbc9be98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673f33a01fbbbed141fd",
+    "url": "/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "f8dfa7b62d6bf872ca22",
+    "url": "/static/js/2.015548f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.015548f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673f33a01fbbbed141fd",
+    "url": "/static/js/main.57b4ec3d.chunk.js"
+  },
+  {
+    "revision": "76fc84219df618cdf232",
+    "url": "/static/js/runtime-main.a26d5d05.js"
+  }
+]);
